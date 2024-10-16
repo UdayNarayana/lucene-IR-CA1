e8928c9ecddf9dc5981f96949c9a38f7bc1e58d0
Uday <udaynl619@gmail.com>
2024-10-16 22:04:07 +0100

    update: change in search file

--- src/main/java/ca/IR/SearchFiles.java
+++ src/main/java/ca/IR/SearchFiles.java
@@ -3,11 +3,12 @@ package ca.IR;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.similarities.*;
+import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.store.FSDirectory;
 
 import java.io.File;
@@ -15,37 +16,13 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class SearchFiles {
-
     public static void main(String[] args) throws Exception {
         String indexPath = args[0];
         String queriesPath = args[1];
-        int scoreType = Integer.parseInt(args[2]);  // Score type parameter
-        String outputPath = args[3];
 
         DirectoryReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
         IndexSearcher searcher = new IndexSearcher(reader);
-
-        // Set similarity based on scoreType
-        switch (scoreType) {
-            case 0:
-                searcher.setSimilarity(new ClassicSimilarity());  // Vector Space Model
-                break;
-            case 1:
-                searcher.setSimilarity(new BM25Similarity());  // BM25
-                break;
-            case 2:
-                searcher.setSimilarity(new BooleanSimilarity());  // Boolean Similarity
-                break;
-            case 3:
-                searcher.setSimilarity(new LMDirichletSimilarity());  // LMDirichlet
-                break;
-            case 4:
-                searcher.setSimilarity(new LMJelinekMercerSimilarity(0.7f));  // LMJelinekMercer
-                break;
-            default:
-                System.out.println("Invalid score type. Please provide a value between 0 and 4.");
-                return;
-        }
+        searcher.setSimilarity(new BM25Similarity());
 
         StandardAnalyzer analyzer = new StandardAnalyzer();
         QueryParser parser = new QueryParser("contents", analyzer);
@@ -53,18 +30,24 @@ public class SearchFiles {
         File queryFile = new File(queriesPath);
         try (Scanner scanner = new Scanner(queryFile)) {
             while (scanner.hasNextLine()) {
-                String queryString = scanner.nextLine();
-                Query query = parser.parse(queryString);
-                System.out.println("Searching for: " + queryString);
-                ScoreDoc[] hits = searcher.search(query, 10).scoreDocs;
+                String queryString = scanner.nextLine().trim(); // Trim whitespace
+                System.out.println("Query String: " + queryString); // Log the query
 
-                for (ScoreDoc hit : hits) {
-                    Document doc = searcher.doc(hit.doc);
-                    System.out.println("Found in file: " + doc.get("filename") + " with score: " + hit.score);
+                try {
+                    Query query = parser.parse(queryString);
+                    System.out.println("Searching for: " + queryString);
+                    ScoreDoc[] hits = searcher.search(query, 10).scoreDocs;
+
+                    for (ScoreDoc hit : hits) {
+                        Document doc = searcher.doc(hit.doc);
+                        System.out.println("Found in file: " + doc.get("filename") + " with score: " + hit.score);
+                    }
+                } catch (ParseException e) {
+                    System.out.println("Failed to parse query: " + queryString);
+                    e.printStackTrace(); // Print the error stack trace
                 }
             }
         }
-
         reader.close();
     }
 }
